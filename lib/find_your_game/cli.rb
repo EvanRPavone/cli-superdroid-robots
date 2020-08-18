0efa4dbeb1da5bed86e07f3774abf0ee55b758ce
EvanRPavone <68548936+EvanRPavone@users.noreply.github.com>
2020-08-18 10:16:46 -0400

    Update cli.rb

--- lib/find_your_game/cli.rb
+++ lib/find_your_game/cli.rb
@@ -1,17 +1,17 @@
 # CLI Controller
-class FindYourGame::CLI
+class SuperDroidRobots::CLI
 
     def call
-      list_games
+      list_robots
       menu
       goodbye
     end
   
-    def list_games
-      puts "Let's Find Your Game:"
-      @games = FindYourGame::Game.all
-      @games.each.with_index(1) do |game, i|
-        puts "#{i}. #{game.name}"
+    def list_robots
+      puts "Let's Take a Look at Superdroid Robots:"
+      @robots = SuperDroidRobots::Game.all
+      @robots.each.with_index(1) do |robot, i|
+        puts "#{i}. #{robot.name}"
       end
 
     end
@@ -19,23 +19,23 @@ class FindYourGame::CLI
     def menu
       input = nil
       while input != "exit"
-        puts "Enter the number of the game to get more info or type list to see the games again or type exit"
+        puts "Enter the number of the robot to get more info or type list to see the robots again or type exit"
         input = gets.strip.downcase
 
         if input.to_i > 0
-          the_game = @games[input.to_i-1]
-          puts "#{the_game.name} - #{the_game.platform} -"
+          the_robot = @robots[input.to_i-1]
+          puts "#{the_robot.name} - #{the_robot.size} -"
         elsif input == "list"
-          list_games
+          list_robots
         else
-          puts "Please put the number related to the game or type exit. If you are a new gamer put 0"
+          puts "Please put the number related to the robot or type exit."
         end
 
       end # Program will not end until you type exit.
     end
   
     def goodbye
-      puts "Start gaming, Gamer!"
+      puts "Thank you for stopping by!"
     end
   
   end
